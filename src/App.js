c453029593dd30211cdbdec49b0023ce09491f40
wdwiramadhan <wahyudwiramadhan@gmail.com>
2020-07-02 23:59:27 +0700

    add header

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import Header from "./components/Header"
 import Container from './components/Container'
 import Subdistrics from './components/Subdistrics'
 import {getSubdistricts} from './services/JeparaServices'
@@ -20,30 +21,29 @@ function App() {
     totalAll = totalPositif + totalRecover + totalDeath;
   }
   return (
-    <div>
+    <div >
+      <Header/>
       <Container>
         <div className="flex flex-col mx-auto max-w-2xl mt-3">
           <div className="flex flex-col content-center sm:flex-row sm:space-x-4 space-y-2">
-            <div className="w-full rounded shadow-md p-5 text-center mt-2">
+            <div className="w-full rounded-md shadow-md p-5 text-center mt-2 bg-white">
               <div className="text-orange-600 text-lg font-bold">{totalPositif}</div>
               <div className="text-gray-700">Positif</div>
             </div>
-            <div className="w-full rounded shadow-md p-5 text-center">
+            <div className="w-full rounded-md shadow-md p-5 text-center bg-white">
               <div className="text-green-600 text-lg font-bold">{totalRecover}</div>
               <div className="text-gray-700">Sembuh</div>
             </div>
-            <div className="w-full rounded shadow-md p-5 text-center">
+            <div className="w-full rounded-md shadow-md p-5 text-center bg-white">
               <div className="text-red-600 text-lg font-bold">{totalDeath}</div>
               <div className="text-gray-700">Meninggal</div>
             </div>
-            <div className="w-full rounded shadow-md p-5 text-center">
+            <div className="w-full rounded-md shadow-md p-5 text-center bg-white">
               <div className="text-yellow-600 text-lg font-bold">{totalAll}</div>
               <div className="text-gray-700">Total</div>
             </div>
           </div>
-          {
-            subdistricts ?  <Subdistrics data={subdistricts}/> : ' '
-          }
+          <Subdistrics data={subdistricts}/> 
         </div>
       </Container>
     </div>
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="mb-5 py-3 shadow-md px-5">
+      <div className="max-w-3xl mx-auto flex-row flex justify-between">
+        <div className="text-xl font-bold text-gray-700 hover:text-gray-600 my-auto">
+              Jepara Covid-19
+          </div>
+        <div className="my-auto text-gray-700">Dark</div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
