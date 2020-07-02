4ea4036763239d7cecda76fa6a1ffded05470ad0
wdwiramadhan <wahyudwiramadhan@gmail.com>
2020-07-02 15:29:40 +0700

    split main app

---
+++ src/components/Subdistrics.js
@@ -0,0 +1,35 @@
+import React from "react"
+
+const Subdistrics = (props) => {
+  const removeKec = name => name.split('').splice(5,1) + name.split('').splice(6).join('').toLowerCase()
+  return (
+    <div className="mt-5 rounded shadow-md py-5 sm:px-5 px-2">
+      <table className="table-fixed w-full">
+        <thead>
+          <tr>
+            <th className="w-2/6 sm:w-1/6 px-1 py-2 text-sm text-gray-800">Kecamatan</th>
+            <th className="w-1/6 px-1 py-2 text-sm text-gray-800">Positif</th>
+            <th className="w-1/6 px-1 py-2 text-sm text-gray-800">Sembuh</th>
+            <th className="w-1/6 px-1 py-2 text-sm text-gray-800">Meninggal</th>
+            <th className="w-1/6 px-1 py-2 text-sm text-gray-800">Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            props.data.map(subdistrict => 
+            <tr key={subdistrict.nama_kec}>
+              <td className="border px-1 py-1 text-gray-700">{removeKec(subdistrict.nama_kec)}</td>
+              <td className="border px-1 py-1 text-center text-gray-700">{subdistrict.covid_all}</td>
+              <td className="border px-1 py-1 text-center text-gray-700">{subdistrict.covid_sembuh}</td>
+              <td className="border px-1 py-1 text-center text-gray-700">{subdistrict.covid_meninggal}</td>
+              <td className="border px-1 py-1 text-center text-gray-700">{parseInt(subdistrict.covid_meninggal) + parseInt(subdistrict.covid_sembuh) + parseInt(subdistrict.covid_all)}</td>
+            </tr>
+            ) 
+          }
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default Subdistrics
