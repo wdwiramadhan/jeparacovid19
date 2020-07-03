06aa7008a5d7f862ee49f0ef0205b4b5e7d9cec7
wdwiramadhan <wahyudwiramadhan@gmail.com>
2020-07-03 22:19:34 +0700

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,8 @@ const Footer = () => {
     <footer>
       <div className="mt-10 text-sm p-3 border-t w-full">
         <div className="max-w-2xl mx-auto flex-row flex justify-between">
-          <div className="w-full text-gray-600 font-light leading-relaxed"><span className="font-medium">Jepara COVID-19 </span> 
-            merupakan website alternative mengenai informasi data COVID-19 di Jepara. Data yang ditampilkan bersumber dari 
+          <div className="w-full text-gray-600 font-light leading-relaxed">
+            Jepara COVID-19 merupakan website alternative mengenai informasi data COVID-19 di Jepara. Data yang ditampilkan bersumber dari 
             <a href="https://corona.jepara.go.id/" className="text-blue-600 hover:text-gray-600"> corona.jepara.go.id</a>
           </div>
         </div>
