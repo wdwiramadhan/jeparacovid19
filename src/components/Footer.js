16674f02419052ecb04e6b8a90ac470da57522a6
wdwiramadhan <wahyudwiramadhan@gmail.com>
2020-07-04 14:40:29 +0700

    change font weight

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,16 +2,14 @@ import React from "react"
 
 const Footer = () => {
   return (
-    <footer>
-      <div className="mt-10 text-sm p-3 border-t w-full">
-        <div className="max-w-2xl mx-auto flex-row flex justify-between">
-          <div className="w-full text-gray-600 font-light leading-relaxed">
-            Jepara COVID-19 merupakan website alternative mengenai informasi data COVID-19 di Jepara. Data yang ditampilkan bersumber dari 
-            <a href="https://corona.jepara.go.id/" className="text-blue-600 hover:text-gray-600"> corona.jepara.go.id</a>
-          </div>
+    <div className="mt-10 text-sm p-3 border-t w-full bg-white">
+      <div className="max-w-2xl mx-auto flex-row flex justify-between">
+        <div className="w-full text-gray-700 leading-relaxed">
+          Jepara COVID-19 merupakan website alternative mengenai informasi data COVID-19 di Jepara. Data yang ditampilkan bersumber dari 
+          <a href="https://corona.jepara.go.id/" className="text-blue-600 hover:text-gray-600"> corona.jepara.go.id</a>
         </div>
       </div>
-    </footer>
+    </div>
   )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,14 @@
 import React from "react";
+import ThemeSwitcher from "../components/ThemeSwticher" 
 
 const Header = () => {
   return (
-    <div className="mb-5 py-3 shadow-md px-5">
+    <div className="mb-5 py-3 shadow-md px-5 bg-white">
       <div className="max-w-3xl mx-auto flex-row flex justify-between">
         <div className="text-xl font-bold text-gray-700 hover:text-gray-600 my-auto">
               Jepara COVID-19
           </div>
-        <div className="my-auto text-gray-700">Dark</div>
+        <div className="my-auto text-gray-700"><ThemeSwitcher/></div>
       </div>
     </div>
   )
