08ade771afef083b010262d9d6efd9776cad22b6
wdwiramadhan <wahyudwiramadhan@gmail.com>
2020-07-21 14:21:43 +0700

    remove dark theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,24 @@
 module.exports = {
   purge: {
+    whitelist: ['mode-dark'],
     content: [
       "./src/**/**/*.js", 
-      "./public/index.html"],
+      "./public/index.html",
+    ],
   },
   theme: {
+    extend: {
+      screens: {
+        dark: { raw: '(prefers-color-scheme: dark)' },
+      },
+    },
   },
-  variants: {},
-  plugins: [],
+  variants: {
+    backgroundColor: ['dark', 'dark-hover', 'dark-group-hover', 'dark-even', 'dark-odd'],
+    borderColor: ['dark', 'dark-focus', 'dark-focus-within'],
+    textColor: ['dark', 'dark-hover', 'dark-active', 'dark-placeholder']
+  },
+  plugins: [
+    require('tailwindcss-dark-mode')()
+  ]
 }
